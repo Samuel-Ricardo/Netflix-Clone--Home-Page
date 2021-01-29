e176926471cd3c085c9fcd35b842788307605949
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-01-29 18:56:20 -0300

    export

--- netflix-app/src/TMDB.js
+++ netflix-app/src/TMDB.js
@@ -1,2 +1,18 @@
 const   API_KEY = '07de60cc832a7924590b50bb4e03689d';
 const API_BASE = 'https://api.themoviedb.org/3';
+
+
+export default {
+
+    getHomeList: async () => {
+
+        return [
+            
+            {
+                slug: 'original',
+                title: 'originais da netflix',
+                items: []
+            }
+        ]
+    }
+}
