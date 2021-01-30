2044497a1d5380ac23562a0ef37563458f41d788
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-01-30 08:57:56 -0300

    update component

--- netflix-app/src/Components/MovieRow/MovieRow.jsx
+++ netflix-app/src/Components/MovieRow/MovieRow.jsx
@@ -5,13 +5,18 @@ export default ({title, itens}) => {
     return(
         <div>
           <h2>{title}</h2>
-          <div className="movieRow--listArea">
-            
+          <div className="MovieRow--ListArea">
+            <div className="MovieRow--List">
+
             {itens.results.length > 0 && itens.results.map((movie, key) => (
 
-              <img src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={itens.original_title}/>
+              <div className="MovieRow--Item">
+                <img src= {`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={itens.original_title}/>
+              </div>
+              
             ))}
 
+            </div>
           </div>
         </div>
     )
