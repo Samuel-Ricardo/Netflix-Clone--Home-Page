39a4f0189490e69d409bf449037860c7f5d42abe
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-05 00:43:24 -0300

    header images

--- netflix-app/src/Components/Header/index.jsx
+++ netflix-app/src/Components/Header/index.jsx
@@ -5,6 +5,24 @@ export default () => {
 
     return (
         
-        
+        <header>
+
+            <div className="Header--Logo">
+
+                <a href="/">
+                    <img src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png'></img>
+                </a>
+
+            </div>
+            
+            <div className="Header--User">
+
+                <a href="/">
+                    <img src= 'https://i.pinimg.com/originals/30/db/47/30db479e1558c3ed46b4ed23b3cd98ae.png'></img>
+                </a>
+
+            </div>
+
+        </header>
     )
 }
