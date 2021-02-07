a9fc26ad16c7a769042bf3cb30b05375a6843694
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-02-07 10:28:12 -0300

    handler left arrow

--- netflix-app/src/Components/MovieRow/index.jsx
+++ netflix-app/src/Components/MovieRow/index.jsx
@@ -5,22 +5,44 @@ import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 
 export default ({title, itens}) => {
-    
+
+  const [scrollX, setScrollX] = React.useState(0)
+  
+  const handlerLeftArrow = () => {
+
+    let x = scrollX + Math.round(window.innerWidth / 2)
+
+    if(x > 0){
+      
+      x = 0
+    }
+
+    setScrollX(x)
+  }
+
+  
+
     return(
         <div className="MovieRow">
 
-          <div className="MovieRow--Right">
+          <div className="MovieRow--Right" onClick={handlerRightArrow}>
             <NavigateNextIcon style={{fontSize: 50}}/>
           </div>
 
-          <div className="MovieRow--Left">
+          <div className="MovieRow--Left" onClick={handlerLeftArrow}>
             <NavigateBeforeIcon style={{fontSize: 50}}/>
           </div>
 
           <h2 className="Title">{title}</h2>
 
           <div className="MovieRow--ListArea">
-            <div className="MovieRow--List">
+            <div className="MovieRow--List"
+            
+              style={{
+                width : itens.results.length * 150,
+                marginLeft: scrollX
+              }}
+            >
 
             {itens.results.length > 0 && itens.results.map((movie, key) => (
 
